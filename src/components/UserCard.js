779c0fb89e3a5496bd63559bd4d30452c0ab3a1e
Ian McGonigle <reverend.magoo@gmail.com>
2021-04-15 01:10:25 -0400

    adding margin to the image on the cards

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,7 +1,7 @@
 export default function UserCard({ avatarUrl, firstName = "" , lastName = "", location = "", teamLead = false }) {
   return (
     <div className='p-4 shadow-md flex flex-col'>
-      <img className="rounded-full" src={`${avatarUrl}`} alt={`${firstName}'s avatar`}/>
+      <img className="rounded-full mb-2" src={`${avatarUrl}`} alt={`${firstName}'s avatar`}/>
       <dl>
         <dt className="font-bold">{`${firstName} ${lastName}`}</dt>
         { teamLead && (<dd className="font-semibold text-sm">Team Lead</dd>) }
